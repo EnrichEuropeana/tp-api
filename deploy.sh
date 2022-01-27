5d2d1edaac489154d96ae031eb0e96a856389b47
trenc <github@tommyschmucker.de>
2022-01-27 11:08:03 +0100

    build: add deploy script for local

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+STAGE=$1
+
+export $(grep -v '^#' .env."$STAGE" | xargs)
+
+echo
+echo "Deploying to $STAGE..."
+date
+
+scp ./dist/tp-api.war $SSH_USER@$SSH_HOST:$SSH_DEST
+
+OUT=$?
+
+if [ $OUT = 0 ]; then
+  echo 'Transfer successful, moving to target...'
+  ssh -t $SSH_USER@$SSH_HOST sudo -s "mv $SSH_DEST/$MV_SRC $MV_DEST"
+  echo 'Deploy successful'
+else
+  echo 'Deploy failed'
+fi
+
+echo
+date
