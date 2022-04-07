eb957fb095400dc90cd8392e856d29ebbd61f62d
trenc <github@tommyschmucker.de>
2022-04-07 10:01:51 +0200

    feat: disable the /projects/test for SOLR full updates on stories and items

--- src/responses/ProjectResponse.java
+++ src/responses/ProjectResponse.java
@@ -330,36 +330,36 @@ public class ProjectResponse {
 
 
 	//Add new entry
-	@Path("/test")
-	@POST
-	public String test(String body) throws SQLException, IOException {
-	    URL storySolr = new URL(PropertiesCache.getInstance().getProperty("SOLR") + "/solr/Stories/dataimport?command=full-import&clean=true");
-	    HttpURLConnection con = (HttpURLConnection) storySolr.openConnection();
-	    con.setRequestMethod("GET");
-		  con.setConnectTimeout(Integer.parseInt(PropertiesCache.getInstance().getProperty("TIMEOUT")));
-	    BufferedReader in = new BufferedReader(
-	    new InputStreamReader(con.getInputStream()));
-	    String inputLine;
-	    StringBuffer content = new StringBuffer();
-	    while ((inputLine = in.readLine()) != null) {
-	        content.append(inputLine);
-	    }
-	    in.close();
-	    con.disconnect();
+	// @Path("/test")
+	// @POST
+	// public String test(String body) throws SQLException, IOException {
+	//     URL storySolr = new URL(PropertiesCache.getInstance().getProperty("SOLR") + "/solr/Stories/dataimport?command=full-import&clean=true");
+	//     HttpURLConnection con = (HttpURLConnection) storySolr.openConnection();
+	//     con.setRequestMethod("GET");
+	// 	  con.setConnectTimeout(Integer.parseInt(PropertiesCache.getInstance().getProperty("TIMEOUT")));
+	//     BufferedReader in = new BufferedReader(
+	//     new InputStreamReader(con.getInputStream()));
+	//     String inputLine;
+	//     StringBuffer content = new StringBuffer();
+	//     while ((inputLine = in.readLine()) != null) {
+	//         content.append(inputLine);
+	//     }
+	//     in.close();
+	//     con.disconnect();
 
-	    URL itemSolr = new URL(PropertiesCache.getInstance().getProperty("SOLR") + "/solr/Items/dataimport?command=full-import&clean=true");
-	    con = (HttpURLConnection) itemSolr.openConnection();
-	    con.setRequestMethod("GET");
-		  con.setConnectTimeout(Integer.parseInt(PropertiesCache.getInstance().getProperty("TIMEOUT")));
-	    in = new BufferedReader(
-	    new InputStreamReader(con.getInputStream()));
-	    content = new StringBuffer();
-	    while ((inputLine = in.readLine()) != null) {
-	        content.append(inputLine);
-	    }
-	    in.close();
-		return content.toString();
-	}
+	//     URL itemSolr = new URL(PropertiesCache.getInstance().getProperty("SOLR") + "/solr/Items/dataimport?command=full-import&clean=true");
+	//     con = (HttpURLConnection) itemSolr.openConnection();
+	//     con.setRequestMethod("GET");
+	// 	  con.setConnectTimeout(Integer.parseInt(PropertiesCache.getInstance().getProperty("TIMEOUT")));
+	//     in = new BufferedReader(
+	//     new InputStreamReader(con.getInputStream()));
+	//     content = new StringBuffer();
+	//     while ((inputLine = in.readLine()) != null) {
+	//         content.append(inputLine);
+	//     }
+	//     in.close();
+	// 	return content.toString();
+	// }
 
 
 	//Edit entry by id
