8bec513e1a6f89107f2a18ec66af089b8c67420a
trenc <github@tommyschmucker.de>
2023-04-18 13:06:13 +0200

    feat: add datedisplay to Person

--- src/objects/Person.java
+++ src/objects/Person.java
@@ -1,23 +1,25 @@
 package objects;
 
 public class Person {
-	public Integer PersonId; 
-	public String FirstName; 
-	public String LastName; 
-	public String BirthPlace; 
+	public Integer PersonId;
+	public String FirstName;
+	public String LastName;
+	public String BirthPlace;
 	public String BirthDate;
-	public String DeathPlace; 
+	public String BirthDateDisplay;
+	public String DeathPlace;
 	public String DeathDate;
-	public String Link; 
-	public String Description; 
+	public String DeathDateDisplay;
+	public String Link;
+	public String Description;
 	public String PersonRole;
-	public Integer ItemId; 
-	
+	public Integer ItemId;
+
 	public void setPersonId (Integer PersonId) {
 		this.PersonId = PersonId;
 	}
-	
-	
+
+
 	public void setFirstName(String firstName) {
 		FirstName = firstName;
 	}
@@ -39,19 +41,27 @@ public class Person {
 	public void setBirthPlace (String BirthPlace) {
 		this.BirthPlace = BirthPlace;
 	}
-	
+
 	public void setBirthDate (String BirthDate) {
 		this.BirthDate = BirthDate;
 	}
-	
+
+	public void setBirthDateDisplay (String BirthDateDisplay) {
+		this.BirthDateDisplay = BirthDateDisplay;
+	}
+
 	public void setDeathPlace (String DeathPlace) {
 		this.DeathPlace = DeathPlace;
 	}
-	
+
 	public void setDeathDate (String DeathDate) {
 		this.DeathDate = DeathDate;
 	}
-	
+
+	public void setDeathDateDisplay (String DeathDateDisplay) {
+		this.DeathDateDisplay = DeathDateDisplay;
+	}
+
 	public void setLink (String Link) {
 		this.Link = Link;
 	}
@@ -59,6 +69,6 @@ public class Person {
 	public void setItemId(Integer itemId) {
 		ItemId = itemId;
 	}
-	
+
 
 }
